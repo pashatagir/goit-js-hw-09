3878b47757f2468f98289b8a41d7d10cb7782adf
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2022-12-03 14:31:02 +0200

    fixed task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+let isActive = false;
+
+refs.btnStart.addEventListener('click', onChangeColorBody);
+refs.btnStop.addEventListener('click', onStopChangeColorBody);
+
+function onChangeColorBody() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  timerId = setInterval(
+    () => (refs.body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+}
+
+function onStopChangeColorBody() {
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

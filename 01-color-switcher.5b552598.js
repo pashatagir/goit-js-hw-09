0f9d602ff715230cdd5956f73134f0c0cdb3316b
pashatagir <108672605+pashatagir@users.noreply.github.com>
2024-02-15 16:28:27 +0000

    Deploying to gh-pages from @ pashatagir/goit-js-hw-09@36a9b092ad18fe29fe71dfc8f29e7f0e1f01fc50 🚀

--- 01-color-switcher.5b552598.js
+++
@@ -1,2 +0,0 @@
-const t={body:document.querySelector("body"),btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};let e=null,r=!1;t.btnStart.addEventListener("click",(function(){if(r)return;r=!0,t.btnStart.setAttribute("disabled",!0),e=setInterval((()=>t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3)})),t.btnStop.addEventListener("click",(function(){t.btnStart.removeAttribute("disabled"),clearInterval(e)}));
-//# sourceMappingURL=01-color-switcher.5b552598.js.map
